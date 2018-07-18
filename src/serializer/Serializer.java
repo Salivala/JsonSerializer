6a67ad755260bc42650b6532a14cf75e040913d9
hsszyman <hsszyman@gmail.com>
2018-07-18 12:29:53 -0400

    Continuing work on deserializer. Will likely use recursive-descent parsing to serialize

--- src/serializer/Serializer.java
+++ src/serializer/Serializer.java
@@ -1,5 +1,6 @@
 package serializer;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 
 public class Serializer {
@@ -7,17 +8,43 @@ public class Serializer {
         Class<?> objClass = obj.getClass();
         StringBuilder jsonBuilder = new StringBuilder();
         jsonBuilder.append("{\n\t\"").append(objClass.getSimpleName()).append("\": {\n");
-        for (Field field : objClass.getDeclaredFields()) {
-            if (!field.getType().isArray()) {
-                jsonBuilder.append("\t\t\"").append(field.getName()).append("\":").append(field.get(obj));
-                jsonBuilder.append("\n");
+        if (!objClass.isPrimitive()) {
+            for (Field field : objClass.getDeclaredFields()) {
+                if (!field.getType().isArray()) {
+                    if (field.get(obj) instanceof String) {
+                        jsonBuilder.append("\t\t\"").append(field.getName()).append("\":").append(field.get(obj));
+                        jsonBuilder.append("\n");
+                    } else if (field.getType().isPrimitive()) {
+                        System.out.println(field.get(obj));
+                    }
+                } else {
+                    jsonBuilder.append("\t\t\"").append(field.getName()).append("\": {\n");
+                    for (Object obje : arrayify(field.get(obj))) {
+                        //jsonBuilder.append(serializeObject(obje));
+                        jsonBuilder.append(serializeObject(obje));
+                    }
+                }
             }
-            else {
-                jsonBuilder.append("\t\t\"").append(field.getName()).append("\": {\n");
-            }
+        }
+        else
+        {
+
         }
         jsonBuilder.append("\t}\n");
         jsonBuilder.append("}");
         return jsonBuilder.toString();
+    }
+
+    /**
+     * PRECONDITION: obj param needs to be categorized as an array in some way
+     * @param obj an object that can be divided into indices
+     * @return the original obj recognized as an array
+     */
+    private static Object[] arrayify(Object obj) {
+        Object[] tempArr = new Object[Array.getLength(obj)];
+        for (int i = 0; i < tempArr.length; i++) {
+            tempArr[i] = Array.get(obj, i);
+        }
+        return tempArr;
     }
 }
